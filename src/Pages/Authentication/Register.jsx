710d548492b0114753174da49d9c9bd52c0f26c6
SwadebDeb sharma <swadebriva2012@gmail.com>
2025-02-17 02:07:39 +0600

    lottie react and firebase setup

--- src/Pages/Authentication/Register.jsx
+++ src/Pages/Authentication/Register.jsx
@@ -1,19 +1,25 @@
+import Lottie from "lottie-react";
+import regLottie from '../../assets/Lottie/register.json'
+
 const Register = () => {
+      const handleRegister = e =>{
+            e.preventDefault();
+            const form = e.target;
+            const email = form.email.value;
+            const password = form.password.value;
+            console.log(email, password)
+      }
+
   return (
     <>
-      <div className="hero bg-base-200 min-h-screen">
+      <div className="hero  min-h-[750px]">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-           
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
+          <div className="text-center lg:text-left w-96">
+            <Lottie animationData={regLottie}></Lottie>
           </div>
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <h1 className="text-4xl font-bold ml-5 pt-5">Register now!</h1>
-            <form className="card-body">
+            <form onSubmit={handleRegister} className="card-body">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
@@ -21,6 +27,7 @@ const Register = () => {
                 <input
                   type="email"
                   placeholder="email"
+                  name="email"
                   className="input input-bordered"
                   required
                 />
@@ -31,6 +38,7 @@ const Register = () => {
                 </label>
                 <input
                   type="password"
+                  name="password"
                   placeholder="password"
                   className="input input-bordered"
                   required
@@ -42,7 +50,7 @@ const Register = () => {
                 </label>
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Register</button>
+                <button type="submit" className="btn btn-primary">Register</button>
               </div>
             </form>
           </div>
